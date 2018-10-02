c8b2added0085aeb1610a16a29f1d167f6fb3d6d
Kimteawan <Defender.cb@gmail.com>
2018-10-02 14:37:36 +0700

    Tạo giao diện chính phần mềm

---
+++ CNPM nhóm 1/Giao Diện Phần Mềm.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CNPM_nhóm_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void thoátToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void quảnLýTàiKhoảnToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dMHàngHóaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void dMNhàCungCấpToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void dMKháchHàngToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void nhậpKhoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void xuấtKhoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void nhậpKhoToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void xuấtKhoToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
